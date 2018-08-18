254edf45028802fa268d2708a0ef60359b06687e
bindergit <binderminder@gmail.com>
2018-08-18 14:43:46 -0500

    Add files via upload

---
+++ hello_world/index.js
@@ -0,0 +1,34 @@
+/*
+ * First homework assignment
+ * A RESTful json api that listens on port 3000
+ * and return a message in JSON format.
+ */
+
+ // Dependecies
+ var http = require('http');
+ var url = require('url');
+ var fs = require('fs');
+
+ // load the greeting
+ var greeting = fs.readFileSync('greeting');
+ // Create the server
+ var httpServer = http.createServer(function(req,res){
+        // Parse the url
+        var parsedUrl = url.parse(req.url, true);
+  
+        // Get the path
+        var path = parsedUrl.pathname;
+        var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+        //trimmedPath = trimmedPath.toLowerCase();
+
+        if (trimmedPath.toLowerCase() === 'hello') {
+            res.end(greeting);    
+        } else {
+            res.end();
+        };
+ 
+});
+// Lesten on port
+httpServer.listen(3000,function(){
+console.log("The server is listening on port 3000");
+});
